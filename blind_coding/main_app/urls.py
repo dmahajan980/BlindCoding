a95897a1017c497639a72caff449269d4afaad59
Aarjav Jain <aarjav00@gmail.com>
2019-12-31 19:57:49 +0530

    Leaderboard Backend

--- blind_coding/main_app/urls.py
+++ blind_coding/main_app/urls.py
@@ -2,15 +2,14 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 from . import views
 
-
 app_name='quiz'
 
 urlpatterns=[
-     path('main/',views.default),
-	 path('',views.index),
-     path('login/',views.login),
-     path('logout/',views.l_out),
-	 path('question/',views.question, name='question'),
-	 path('main/runCode/',views.runCode, name='runCode')
-
+     path('main/', views.default, name='main'),
+     path('', views.index, name='index'),
+     path('login/', views.login, name='login'),
+     path('logout/', views.l_out, name='logout'),
+     path('question/', views.question, name='question'),
+     path('main/runCode/', views.runCode, name='runCode'),
+     path('leaderboard/', views.leaderboard, name='leaderboard')
 ]
