1a009bb09ce9bb7f75a149aeb0a1385888c114ed
HarpinderJotSingh <harpinderjot36@gmail.com>
2020-01-04 18:37:15 +0530

    Changed compiler api from jdoodle to judge0

--- blind_coding/static/js/app.js
+++ blind_coding/static/js/app.js
@@ -1,11 +1,12 @@
 // var request = require('request');
 
 $(document).ready(function() {
+  populateLangs();
 	var inp = document.getElementsByClassName('noselect')[0];
-getQuestion(0);
-inp.addEventListener('select', function() {
-  this.selectionStart = this.selectionEnd;
-}, false);
+  getQuestion(0);
+  inp.addEventListener('select', function() {
+    this.selectionStart = this.selectionEnd;
+  }, false);
 	
 	document.addEventListener('contextmenu', event => event.preventDefault());
     var ctrlDown = false,
@@ -70,6 +71,23 @@ let qNo = 0;
 let tc1 = '';
 let tc2 = '';
 let tc3 = '';
+let languageIDs = JSON.parse("[{\"id\":45,\"name\":\"Assembly (NASM 2.14.02)\"},{\"id\":46,\"name\":\"Bash (5.0.0)\"},{\"id\":47,\"name\":\"Basic (FBC 1.07.1)\"},{\"id\":48,\"name\":\"C (GCC 7.4.0)\"},{\"id\":52,\"name\":\"C++ (GCC 7.4.0)\"},{\"id\":49,\"name\":\"C (GCC 8.3.0)\"},{\"id\":53,\"name\":\"C++ (GCC 8.3.0)\"},{\"id\":50,\"name\":\"C (GCC 9.2.0)\"},{\"id\":54,\"name\":\"C++ (GCC 9.2.0)\"},{\"id\":51,\"name\":\"C# (Mono 6.6.0.161)\"},{\"id\":55,\"name\":\"Common Lisp (SBCL 2.0.0)\"},{\"id\":56,\"name\":\"D (DMD 2.089.1)\"},{\"id\":57,\"name\":\"Elixir (1.9.4)\"},{\"id\":58,\"name\":\"Erlang (OTP 22.2)\"},{\"id\":44,\"name\":\"Executable\"},{\"id\":59,\"name\":\"Fortran (GFortran 9.2.0)\"},{\"id\":60,\"name\":\"Go (1.13.5)\"},{\"id\":61,\"name\":\"Haskell (GHC 8.8.1)\"},{\"id\":62,\"name\":\"Java (OpenJDK 13.0.1)\"},{\"id\":63,\"name\":\"JavaScript (Node.js 12.14.0)\"},{\"id\":64,\"name\":\"Lua (5.3.5)\"},{\"id\":65,\"name\":\"OCaml (4.09.0)\"},{\"id\":66,\"name\":\"Octave (5.1.0)\"},{\"id\":67,\"name\":\"Pascal (FPC 3.0.4)\"},{\"id\":68,\"name\":\"PHP (7.4.1)\"},{\"id\":43,\"name\":\"Plain Text\"},{\"id\":69,\"name\":\"Prolog (GNU Prolog 1.4.5)\"},{\"id\":70,\"name\":\"Python (2.7.17)\"},{\"id\":71,\"name\":\"Python (3.8.1)\"},{\"id\":72,\"name\":\"Ruby (2.7.0)\"},{\"id\":73,\"name\":\"Rust (1.40.0)\"},{\"id\":74,\"name\":\"TypeScript (3.7.4)\"}]");
+
+function populateLangs()
+{
+  console.log('populating languages...');
+  let selectField = document.getElementById('langSelect');
+  for(element of languageIDs)
+  {
+    console.log('adding.. ',element);
+     var opt = document.createElement("option");
+     opt.value= element['id'];
+     opt.innerHTML = element['name']; // whatever property it has
+  
+     // then append it to the select element
+     selectField.appendChild(opt);
+  }
+}
 
 function setCode(prog){
   code = prog;
@@ -104,7 +122,7 @@ function getCustomInput(){
 }
 
 function setOutput(outp) {
-  output = outp['output'];
+  output = outp['stdout'];
 }
 function getOutput(){
   return output 
@@ -128,6 +146,7 @@ function runCode(){
 
   // Get language chosen by the user and store it
   let lang = document.getElementById("langSelect").value;
+  console.log('langCode: ', lang);
   // setLanguage(lang);
 
   // console.log('Language: ', getLanguage(), '\nCode: ', getCode());
@@ -139,9 +158,9 @@ function runCode(){
       // Code equals script
       // script : getCode(),
       // language: getLanguage(),
-      script : prog,
-      language: lang,
-      versionIndex: versions[versionNo],
+      source_code : prog,
+      language_id: lang,
+      // versionIndex: versions[versionNo],
       stdin: getCustomInput(), //to give custom input
       qNo: getQNum(),
       timeElapsed: time
